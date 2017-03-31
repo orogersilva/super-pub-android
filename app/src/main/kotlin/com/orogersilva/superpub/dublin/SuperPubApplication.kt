5cf7f0284a996679e6edb4855aba937b165de21e
Roger Silva <orogersilva@gmail.com>
2017-03-31 14:49:53 -0300

    Converted Java to Kotlin. :D

---
+++ app/src/main/kotlin/com/orogersilva/superpub/dublin/SuperPubApplication.kt
@@ -0,0 +1,18 @@
+package com.orogersilva.superpub.dublin
+
+import android.app.Application
+
+/**
+ * Created by orogersilva on 3/31/2017.
+ */
+class SuperPubApplication : Application() {
+
+    // region APPLICATION LIFECYCLE METHODS
+
+    override fun onCreate() {
+
+        super.onCreate()
+    }
+
+    // endregion
+}
---
+++ app/src/main/kotlin/com/orogersilva/superpub/dublin/model/Pub.kt
@@ -0,0 +1,7 @@
+package com.orogersilva.superpub.dublin.model
+
+/**
+ * Created by orogersilva on 3/31/2017.
+ */
+class Pub {
+}
