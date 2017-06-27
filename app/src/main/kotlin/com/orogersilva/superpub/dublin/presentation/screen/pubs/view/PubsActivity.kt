a5cc489b89d01e188f693e322882edd555954efb
Roger Silva <orogersilva@gmail.com>
2017-06-27 16:16:12 -0300

    Implemented permissions request mechanism for Marshmallow version.

--- app/src/main/kotlin/com/orogersilva/superpub/dublin/presentation/screen/pubs/view/PubsActivity.kt
+++ app/src/main/kotlin/com/orogersilva/superpub/dublin/presentation/screen/pubs/view/PubsActivity.kt
@@ -1,28 +1,33 @@
 package com.orogersilva.superpub.dublin.presentation.screen.pubs.view
 
 import android.Manifest
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import android.support.v4.app.ActivityCompat
+import android.provider.Settings
+import android.support.v4.app.DialogFragment
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.RecyclerView
 import com.orogersilva.superpub.dublin.R
 import com.orogersilva.superpub.dublin.di.component.PubsActivityComponent
 import com.orogersilva.superpub.dublin.di.module.*
-import com.orogersilva.superpub.dublin.domain.model.Pub
 import com.orogersilva.superpub.dublin.presentation.model.PubModel
-import com.orogersilva.superpub.dublin.presentation.screen.pubdetails.view.PubDetailsActivity
 import com.orogersilva.superpub.dublin.presentation.screen.pubs.PubsContract
 import com.orogersilva.superpub.dublin.presentation.screen.pubs.adapter.PubsAdapter
+import com.orogersilva.superpub.dublin.shared.PermissionUtils
 import com.orogersilva.superpub.dublin.shared.app
 import com.orogersilva.superpub.dublin.shared.hasPermission
-import com.orogersilva.superpub.dublin.shared.permissionsHasBeenGranted
 import kotlinx.android.synthetic.main.activity_pubs.*
 import javax.inject.Inject
 
 /**
  * Created by orogersilva on 4/13/2017.
  */
-class PubsActivity : AppCompatActivity(), PubsContract.View {
+class PubsActivity : AppCompatActivity(), PubsContract.View,
+        PermissionUtils.PermissionDeniedDialog.PermissionDeniedDialogListener,
+        PermissionUtils.RationaleDialog.RationaleDialogListener {
 
     // region PROPERTIES
 
@@ -35,6 +40,10 @@ class PubsActivity : AppCompatActivity(), PubsContract.View {
 
 
     private val ACCESS_LOCATION_PERMISSION_REQUEST_CODE = 1
+    private val APPLICATION_DETAILS_SETTINGS_REQUEST_CODE = 2
+
+    private lateinit var permissionDeniedDialog: DialogFragment
+    private lateinit var rationaleDialog: DialogFragment
 
     private var hasPermissionToAccessDeviceLocation = false
 
@@ -63,28 +72,30 @@ class PubsActivity : AppCompatActivity(), PubsContract.View {
 
         pubsSwipeRefreshLayout.setOnRefreshListener { pubsPresenter.resume() }
         pubsSwipeRefreshLayout.setColorSchemeColors(
-                resources.getColor(R.color.gold), resources.getColor(R.color.black)
+                ContextCompat.getColor(this, R.color.gold), ContextCompat.getColor(this, R.color.black)
         )
+    }
 
-        if (!hasPermission(Manifest.permission.ACCESS_COARSE_LOCATION) &&
-                !hasPermission(Manifest.permission.ACCESS_FINE_LOCATION)) {
+    override fun onStart() {
 
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.ACCESS_COARSE_LOCATION) ||
-                    ActivityCompat.shouldShowRequestPermissionRationale(this,
-                            Manifest.permission.ACCESS_FINE_LOCATION)) {
+        super.onStart()
 
-                // TODO: SHOULD BE IMPLEMENTED EXPLANATION TO THE USE ABOUT WHY TO USE LOCATION FROM DEVICE.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            if (!hasPermission(Manifest.permission.ACCESS_COARSE_LOCATION) &&
+                    !hasPermission(Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                PermissionUtils.requestPermissions(this, ACCESS_LOCATION_PERMISSION_REQUEST_CODE,
+                        listOf(Manifest.permission.ACCESS_COARSE_LOCATION,
+                                Manifest.permission.ACCESS_FINE_LOCATION), getString(
+                        R.string.location_permission_rationale_dialog_title), getString(
+                        R.string.location_permission_rationale_dialog_message), getString(R.string.yes),
+                        getString(R.string.cancel), false)
 
             } else {
 
-                ActivityCompat.requestPermissions(this,
-                        arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION,
-                                Manifest.permission.ACCESS_FINE_LOCATION),
-                        ACCESS_LOCATION_PERMISSION_REQUEST_CODE)
+                hasPermissionToAccessDeviceLocation = true
             }
-
-            return
 
         } else {
 
@@ -141,22 +152,81 @@ class PubsActivity : AppCompatActivity(), PubsContract.View {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
+    override fun onPermissionDeniedDialogEnablingButtonClicked() {
+
+        showApplicationDetailsSettingsPane()
+    }
+
+    override fun onRationaleDialogPositiveButtonClicked(requestCode: Int, permissions: List<String>) {
+
+        PermissionUtils.requestPermissionsWithoutAskForRequestPermissionRationale(this, permissions, requestCode)
+    }
+
+    override fun onRationaleDialogNegativeButtonClicked() {
+
+        showPermissionDeniedDialog()
+    }
+
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
 
-        when (requestCode) {
+        if (requestCode != ACCESS_LOCATION_PERMISSION_REQUEST_CODE) return
 
-            ACCESS_LOCATION_PERMISSION_REQUEST_CODE -> {
+        if (PermissionUtils.isPermissionsGranted(permissions.toList(), grantResults.toList())) {
 
-                if (permissionsHasBeenGranted(grantResults)) {
+            pubsPresenter.resume()
 
-                    pubsPresenter.resume()
+        } else {
 
-                } else {
+            if (PermissionUtils.shouldShowRequestPermissionRationale(this, permissions.toList())) {
 
-                    // TODO: Denied permission. To implement.
-                }
+                showPermissionRationaleDialog(permissions.toList(), requestCode)
+
+            }else {
+
+                // "Don't show again" has been checked.
+                showPermissionDeniedDialog()
             }
         }
+    }
+
+    // endregion
+
+    // region UTILITY METHODS
+
+    private fun showPermissionRationaleDialog(permissions: List<String>, requestCode: Int) {
+
+        rationaleDialog = PermissionUtils.RationaleDialog.newInstance(getString(
+                R.string.location_permission_rationale_dialog_title),
+                getString(R.string.location_permission_rationale_dialog_message),
+                getString(R.string.yes), getString(R.string.cancel),
+                permissions.toList(), requestCode, false)
+
+        supportFragmentManager
+                .beginTransaction()
+                .add(rationaleDialog, null)
+                .commitAllowingStateLoss()
+    }
+
+    private fun showPermissionDeniedDialog() {
+
+        permissionDeniedDialog = PermissionUtils.PermissionDeniedDialog.newInstance(
+                getString(R.string.location_denied_permission_dialog_title),
+                getString(R.string.location_denied_permission_dialog_message),
+                getString(R.string.location_enabling_label), false)
+
+        supportFragmentManager
+                .beginTransaction()
+                .add(permissionDeniedDialog, null)
+                .commitAllowingStateLoss()
+    }
+
+    private fun showApplicationDetailsSettingsPane() {
+
+        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+
+        intent.data = Uri.fromParts("package", packageName, null)
+
+        startActivityForResult(intent, APPLICATION_DETAILS_SETTINGS_REQUEST_CODE)
     }
 
     // endregion
---
+++ app/src/main/kotlin/com/orogersilva/superpub/dublin/shared/PermissionUtils.kt
@@ -0,0 +1,300 @@
+package com.orogersilva.superpub.dublin.shared
+
+import android.app.Dialog
+import android.content.Context
+import android.content.DialogInterface
+import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.app.DialogFragment
+import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by orogersilva on 6/27/2017.
+ */
+object PermissionUtils {
+
+    // region PUBLIC METHODS
+
+    fun requestPermissions(activity: AppCompatActivity, requestCode: Int, permissions: List<String>,
+                           permissionRationaleDialogTitle: String, permissionRationaleDialogMessage: String,
+                           permissionRationaleDialogPositiveButtonLabel: String,
+                           permissionRationaleDialogNegativeButtonLabel: String,
+                           finishActivity: Boolean) {
+
+        if (shouldShowRequestPermissionRationale(activity, permissions)) {
+
+            val rationaleDialog = PermissionUtils.RationaleDialog.newInstance(
+                    permissionRationaleDialogTitle, permissionRationaleDialogMessage,
+                    permissionRationaleDialogPositiveButtonLabel, permissionRationaleDialogNegativeButtonLabel,
+                    permissions, requestCode, finishActivity)
+
+            activity.supportFragmentManager
+                    .beginTransaction()
+                    .add(rationaleDialog, null)
+                    .commitAllowingStateLoss()
+
+        } else {
+
+            ActivityCompat.requestPermissions(activity, permissions.toTypedArray(), requestCode)
+        }
+    }
+
+    fun requestPermissionsWithoutAskForRequestPermissionRationale(activity: AppCompatActivity, permissions: List<String>, requestCode: Int) {
+
+        ActivityCompat.requestPermissions(activity, permissions.toTypedArray(), requestCode)
+    }
+
+    fun isPermissionsGranted(permissions: List<String>, grantResults: List<Int>): Boolean {
+
+        permissions.forEach {
+
+            var permissionGranted = false
+
+            for (i in permissions.indices) {
+
+                if (it.equals(grantResults[i])) {
+
+                    permissionGranted = true
+
+                    break
+                }
+            }
+
+            if (!permissionGranted) return false
+        }
+
+        return true
+    }
+
+    fun shouldShowRequestPermissionRationale(activity: AppCompatActivity, permissions: List<String>): Boolean {
+
+        permissions.forEach {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(activity, it)) return true
+        }
+
+        return false
+    }
+
+    // endregion
+
+    // region CLASSES
+
+    class RationaleDialog : DialogFragment() {
+
+        // region PROPERTIES
+
+        private lateinit var rationaleDialogListener: RationaleDialogListener
+
+        private lateinit var title: String
+        private lateinit var message: String
+        private lateinit var positiveButtonLabel: String
+        private lateinit var negativeButtonLabel: String
+        private lateinit var permissions: List<String>
+
+        private var finishActivity = false
+
+        // endregion
+
+        // region INTERFACES
+
+        interface RationaleDialogListener {
+
+            // region METHODS
+
+            fun onRationaleDialogPositiveButtonClicked(requestCode: Int, permissions: List<String>)
+
+            fun onRationaleDialogNegativeButtonClicked()
+
+            // endregion
+        }
+
+        // endregion
+
+        // region FACTORY METHODS
+
+        companion object {
+
+            fun newInstance(title: String, message: String, positiveButtonLabel: String,
+                            negativeButtonLabel: String, permissions: List<String>, requestCode: Int,
+                            finishActivity: Boolean): DialogFragment {
+
+                val arguments = Bundle()
+
+                arguments.putInt("request_code", requestCode)
+                arguments.putBoolean("finish_activity", finishActivity)
+
+                val dialog = RationaleDialog()
+
+                dialog.title = title
+                dialog.message = message
+                dialog.positiveButtonLabel = positiveButtonLabel
+                dialog.negativeButtonLabel = negativeButtonLabel
+
+                dialog.permissions = permissions
+
+                dialog.arguments = arguments
+
+                return dialog
+            }
+        }
+
+        // endregion
+
+        // region OVERRIDED METHODS
+
+        override fun onAttach(context: Context?) {
+
+            super.onAttach(context)
+
+            try {
+
+                rationaleDialogListener = context as RationaleDialogListener
+
+            } catch (e: ClassCastException) {
+
+                throw ClassCastException(context.toString() + " must implement RationaleDialogListener.")
+            }
+        }
+
+        override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+
+            val arguments = arguments
+
+            val requestCode = arguments.getInt("request_code")
+            finishActivity = arguments.getBoolean("finish_activity")
+
+            val alertDialogBuilder = AlertDialog.Builder(activity)
+                    .setTitle(title)
+                    .setMessage(message)
+                    .setPositiveButton(positiveButtonLabel, object : DialogInterface.OnClickListener {
+
+                        override fun onClick(dialogInterface: DialogInterface?, id: Int) {
+
+                            rationaleDialogListener.onRationaleDialogPositiveButtonClicked(requestCode, permissions)
+                        }
+                    })
+                    .setNegativeButton(negativeButtonLabel, object : DialogInterface.OnClickListener {
+
+                        override fun onClick(dialogInterface: DialogInterface?, id: Int) {
+
+                            rationaleDialogListener.onRationaleDialogNegativeButtonClicked()
+                        }
+                    })
+
+            return alertDialogBuilder.create()
+        }
+
+        override fun dismiss() {
+
+            super.dismiss()
+
+            if (finishActivity) activity.finish()
+        }
+
+        // endregion
+    }
+
+    class PermissionDeniedDialog private constructor() : DialogFragment() {
+
+        // region PROPERTIES
+
+        private lateinit var permissionDeniedDialogListener: PermissionDeniedDialogListener
+
+        private lateinit var title: String
+        private lateinit var message: String
+        private lateinit var enableButtonLabel: String
+
+        private var finishActivity = false
+
+        // endregion
+
+        // region INTERFACES
+
+        interface PermissionDeniedDialogListener {
+
+            // region METHODS
+
+            fun onPermissionDeniedDialogEnablingButtonClicked()
+
+            // endregion
+        }
+
+        // endregion
+
+        // region FACTORY METHODS
+
+        companion object {
+
+            // region METHODS
+
+            fun newInstance(title: String, message: String, enableButtonLabel: String,
+                            finishActivity: Boolean): DialogFragment {
+
+                val arguments = Bundle()
+
+                arguments.putBoolean("finish_activity", finishActivity)
+
+                val dialog = PermissionDeniedDialog()
+
+                dialog.title = title
+                dialog.message = message
+                dialog.enableButtonLabel = enableButtonLabel
+
+                dialog.arguments = arguments
+
+                return dialog
+            }
+
+            // endregion
+        }
+
+        // endregion
+
+        // region OVERRIDED METHODS
+
+        override fun onAttach(context: Context?) {
+
+            super.onAttach(context)
+
+            try {
+
+                permissionDeniedDialogListener = context as PermissionDeniedDialogListener
+
+            } catch (e: ClassCastException) {
+
+                throw ClassCastException(context.toString() + " must implement PermissionDeniedDialogListener.")
+            }
+        }
+
+        override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+
+            finishActivity = arguments.getBoolean("finish_activity")
+
+            val alertDialogBuilder = AlertDialog.Builder(activity)
+                    .setTitle(title)
+                    .setMessage(message)
+                    .setPositiveButton(enableButtonLabel, object : DialogInterface.OnClickListener {
+
+                        override fun onClick(dialogInterface: DialogInterface?, id: Int) {
+
+                            permissionDeniedDialogListener.onPermissionDeniedDialogEnablingButtonClicked()
+                        }
+                    })
+
+            return alertDialogBuilder.create()
+        }
+
+        override fun onDismiss(dialog: DialogInterface?) {
+
+            super.onDismiss(dialog)
+
+            if (finishActivity) activity.finish()
+        }
+
+        // endregion
+    }
+
+    // endregion
+}
